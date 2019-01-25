f55b5b4f48203278f1376d77e1b999c18fdb5073
Shikhar Khandelwal <conveytoshikhar@gmail.com>
2019-01-26 03:18:29 +0800

    Website shifted to react, actual dev begins now

---
+++ public/javascripts/index.js
@@ -0,0 +1,53 @@
+/** 
+ * ===================================================================
+ * Main js
+ *
+ * ------------------------------------------------------------------- 
+ */ 
+
+(function($) {
+
+	"use strict";
+
+
+	/* ----------
+	Loading Page
+	----------*/
+	var counter = 0;
+	var c = 0;
+    var perfData = window.performance.timing // The PerformanceTiming interface
+    var estimatedTime = -(perfData.loadEventEnd - perfData.navigationStart) // Calculated Estimated Time of Page Load which returns negative value.
+	var time = parseInt((estimatedTime/1000)%60)*100; //Converting EstimatedTime from miliseconds to seconds.)
+	var i = setInterval(function(){
+		$(".loading-page .counter p").html(counter + "%");
+		$(".loading-page .counter hr").css("visibility", "visible");
+        $(".loading-page .counter hr").css("width", counter+ "%");
+        $(".loading-page .counter").css("background", "linear-gradient(to right, #f60d54 "+ c + "%,#0d0d0d "+ c + "%)");
+  
+      counter++;
+      if(counter > 100) {
+		  counter=0;
+		  clearInterval(i);
+		  $('.loading-page').fadeOut("slow","linear",()=>{
+			
+			$("#home").css("display","block").hide().fadeIn(3000);
+			$('.home-particles').particleground({
+				dotColor: '#fff',
+				lineColor: '#555555',
+				particleRadius: 6,
+				curveLines: true,
+				density: 10000,
+				proximity: 110
+			 }); 
+		  })
+		 
+      }
+	}, estimatedTime/100);
+	
+
+	/* --------------------------------------------------- */
+	/*  Particle JS
+	------------------------------------------------------ */
+	
+	
+})(jQuery);
---
+++ src/components/Home/Home.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './tempMain.css';
+
+
+class Home extends Component {
+ 
+  render() {
+    return (
+        <div>   
+            <div className="loading-page">
+                <div className="counter">
+                    <h1>LOADING</h1>
+                    <p>0%</p>
+                    <hr/>
+                </div>
+            </div>
+            <section id="home" className="home-particles">
+                
+            </section>
+        </div>
+        
+    );
+  }
+}
+
+export default Home;
