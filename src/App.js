8eeef7bf09dae7b17a0346a6bb75311462599c7c
Shikhar Khandelwal <conveytoshikhar@gmail.com>
2019-01-28 17:16:06 +0800

    About page started

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './main.css';
 import Home from './components/Home/Home';
+import About from './components/About/About';
 
 
 
 class App extends Component {
   render() {
     return (
-        <Home />
-    );
+        <div>
+          {/* <Home /> */}
+          <About />
+        </div> 
+    )
   }
 }
 
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import './home.css';
+import Header from "../Header/Header";
+import './about.css';
 
 
-class Home extends Component {
+class About extends Component {
  
   render() {
     return (
-        <div>   
-            
+        <div class="first-page">
+          <Header />
+            <h1 class="intro">
+              We are a software solution agency based in the city of Hong Kong.
+              We devote ourselves towards realising your ideas by listening, asking, researching, thinking 
+              and dissecting before we recreate powerful ideas in the form of impactful softwares that help your 
+              brand narrate its unique ideas and reach out millions.
+            </h1> 
+            <p class="services-offered">
+              Web Apps / Mobile Apps/ In-House Softwares/ Backend Management Services / Software Transformation Services
+            </p>
         </div>
         
     );
   }
 }
 
-export default Home;
+export default About;
