390152fc810745fd804424cf3766b81ab4099598
Shikhar Khandelwal <conveytoshikhar@gmail.com>
2019-02-03 17:16:19 +0800

    Added shikhar description

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -38,14 +38,11 @@ class About extends Component {
                         <div class="text-details">
                             <h3>Application Devloper</h3>
                             <h1> Shikhar Khandelwal</h1>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-                                has been the industry's standard dummy text ever since the 1500s,
-                                when an unknown printer took a galley of type and scrambled it to make a type specimen
-                                book.
-                                <br/><br/>It has survived not only five centuries, but also the leap into electronic
-                                typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-                                release of Letraset sheets containing Lorem Ipsum passages, and more recently with
-                                desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                            <p> Shikhar is our Application Developer. He has been developing applications for almost three years
+                                and has never failed to provide a software that is commendable in all aspects. Focusing on optimising 
+                                already created softwares, to creating efficient applications that benefit the society, Shikhar loves the 
+                                development area of computer science. 
+                                <br/><br/>On a less serious note, he loves playing basketball and listening to music. Guy is like a 'White Iverson'!</p>
                         </div>
                     </div>
                     <div className="image-poster"><img src={shikhar} alt="developer"/></div>
